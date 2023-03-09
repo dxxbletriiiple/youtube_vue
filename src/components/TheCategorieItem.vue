814af140f2b94b8ff50ff19dba914d568cd3f799
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-09 07:46:35 +0300

    add: category item

---
+++ src/components/TheCategorieItem.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ text: string }>();
+/* export default {
+	props: ['text'],
+}; */
+</script>
+
+<template>
+	<a href="#" class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white">{{ text }}</a>
+</template>
