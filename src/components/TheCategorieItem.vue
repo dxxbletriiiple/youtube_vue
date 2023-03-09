0fb57eee73fb20fcab63a2b362748d330dab6d47
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-09 21:58:22 +0300

    add: active class to category item

--- src/components/TheCategorieItem.vue
+++ src/components/TheCategorieItem.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-defineProps<{ text: string }>();
-/* export default {
-	props: ['text'],
-}; */
+defineProps<{ text: string; isActive: boolean }>();
 </script>
 
 <template>
-	<a href="#" class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white">{{ text }}</a>
+	<a
+		href="#"
+		class="px-3 py-1 transition rounded-full"
+		:class="{
+			'bg-gray-600 border border-gray-700 hover:bg-gray-500 text-white': isActive,
+		}"
+		>{{ text }}</a
+	>
 </template>
--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -40,7 +40,7 @@ export default {
 	<section class="pt-14 md:pl-24 xl:pl-64 w-full fixed bg-white bg-opacity-95 z-10">
 		<div class="border-t border-b px-4 max-w-screen-2xl m-auto">
 			<div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap scrollbar">
-				<TheCategorieItem v-for="text of texts" :key="text" :text="text" />
+				<TheCategorieItem v-for="text of texts" :key="text" :text="text" :isActive="text == 'All'" />
 			</div>
 		</div>
 	</section>
