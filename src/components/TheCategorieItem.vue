d1388f0f212e401e1b03dee859614333b557867e
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-09 22:15:20 +0300

    add: classes for not cative elements

--- src/components/TheCategorieItem.vue
+++ src/components/TheCategorieItem.vue
@@ -5,9 +5,10 @@ defineProps<{ text: string; isActive: boolean }>();
 <template>
 	<a
 		href="#"
-		class="px-3 py-1 transition rounded-full"
+		class="px-3 py-1 transition rounded-full border"
 		:class="{
-			'bg-gray-600 border border-gray-700 hover:bg-gray-500 text-white': isActive,
+			'bg-gray-600 border-gray-700 hover:bg-gray-500 text-white': isActive,
+			'bg-gray-100  border-gray-300 hover:bg-gray-200': !isActive,
 		}"
 		>{{ text }}</a
 	>
